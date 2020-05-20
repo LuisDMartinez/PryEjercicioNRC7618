aca7840774fbb98fefedfea70edf5be292ad1586
LuisD06 <luisdmartinezu06@gmail.com>
2020-05-20 11:11:10 -0500

    Primera version del codigo

---
+++ src/main/java/com/martinez/ejercicio/PryEjercicio1Application.java
@@ -0,0 +1,14 @@
+package com.martinez.ejercicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//todo @ sobre una calse o atributo es una anotacion
+@SpringBootApplication
+public class PryEjercicio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PryEjercicio1Application.class, args);
+	}
+
+}
